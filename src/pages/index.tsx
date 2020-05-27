e1b81ffb7b2f984d61363a2bcac615fabb10d450
csobr <siham.hadi@live.se>
2020-05-27 21:03:36 +0200

    Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { homedir } from "os";
+import * as React from 'react';
+import { homedir } from 'os';
 
 const Home: React.FC = ({}) => {
   return <div></div>;
