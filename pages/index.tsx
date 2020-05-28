ce4bf6e71fe30371aef621072ebe586e43acd109
csobr <siham.hadi@live.se>
2020-05-28 15:42:13 +0200

    first view

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,27 @@ import Link from 'next/link';
 const Home: React.FC = ({}) => {
   return (
     <main className="main">
-      <Link href="/">
-        <a>Om</a>
-      </Link>
-      <Link href="/">
-        <a>Ordna</a>
-      </Link>
-      <h1>Lorem ipsum dolor sit amet</h1>
-      <img src="./images/inred.jpg"></img>
+      <nav>
+        <Link href="/">
+          <a>Om</a>
+        </Link>
+        <Link href="/">
+          <a>Ordna</a>
+        </Link>
+      </nav>
+      <div className="first-view">
+        <div className="header">
+          <h1>Lorem ipsum dolor sit amet feugiat tempor</h1>
+        </div>
+        <div className="first-img">
+          <img className="pink" src="./images/inred.jpg" alt="pink"></img>
+          <p className="title">Lorem ipsum dolor sit amet, consectetur.</p>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet feugiat tempor, ipsum, facilisi aliquet.
+            Tortor amet nulla semper enim, praesent sit tristique.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
