ceedd0773772c774974e0d14938696274c95c6a0
csobr <siham.hadi@live.se>
2020-05-28 11:03:27 +0200

    New files

---
+++ src/pages/Area.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+const Area: React.FC = ({}) => {
+  return <div></div>;
+};
+export default Area;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
 import Home from './Home';
+import About from './About';
+import Area from './Area';
 
-export default Home;
+export default { Home, About, Area };
