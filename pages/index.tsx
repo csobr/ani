2545e9b171a9f6c7c783f2aa72eae669e5d72b08
csobr <siham.hadi@live.se>
2021-01-09 19:39:59 +0100

    first view skeleton WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,22 @@ const Home: React.FC = ({}) => {
         <a>ani</a>
         <a>Om</a>
       </nav>
-     
+      <section className="first-view">
+        <div className="header">
+          <h1>Min tonårs-{'\n'}hjärna</h1>
+        </div>
+        <div className="shapes">
+          <div className="rectangle" />
+          <div className="triangel" />
+          <div className="circle" />
+          <div className = "sqaure"/>
+        </div>
+       
+        <p>Under tonårstiden sker det många förändringar i livet. Man kommer in puberteten och kroppen förändras 
+mycket. Även hjärnan utvecklas under 
+den här tiden. Förstå vad som händer i
+din hjärna med hjälp av forsking.</p>
+     </section>
     </main>
   );
 };
