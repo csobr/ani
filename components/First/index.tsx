8b7a0b61a602f3d17a0f0271d65401b1ec4c8638
csobr <siham.hadi@live.se>
2021-01-10 21:10:32 +0100

    second view WIP

---
+++ components/First/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const FirstItem: React.FC = ({}) => {
+  return (
+
+      <section className="first-view">
+        <div className="header">
+          <h1>Min tonårs-{'\n'}hjärna</h1>
+        </div>
+        <div className="shapes">
+          <div className="rectangle" />
+          <div className="triangel" />
+          <div className="circle" />
+          <div className="sqaure" />
+        </div>
+        <div className= "app-description">
+        <p>
+            Under tonårstiden sker det många förändringar i livet. Man kommer in puberteten och kroppen förändras mycket.
+            Även hjärnan utvecklas under den här tiden. Förstå vad som händer i din hjärna med hjälp av forsking.
+        </p>
+        </div>
+      </section>
+
+  );
+};
+
+export default FirstItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import FirstItem from '@components/First';
+import SecondItem from '@components/Second';
 import React from 'react';
 
 const Home: React.FC = ({}) => {
@@ -7,24 +9,8 @@ const Home: React.FC = ({}) => {
         <img src = "logo.png" alt = "logo"></img>
         <a>Om</a>
       </nav>
-      <section className="first-view">
-        <div className="header">
-          <h1>Min tonårs-{'\n'}hjärna</h1>
-        </div>
-        <div className="shapes">
-          <div className="rectangle" />
-          <div className="triangel" />
-          <div className="circle" />
-          <div className="sqaure" />
-        </div>
-        <div className= "app-description">
-        <p>
-            Under tonårstiden sker det många förändringar i livet. Man kommer in puberteten och kroppen förändras mycket.
-            Även hjärnan utvecklas under den här tiden. Förstå vad som händer i din hjärna med hjälp av forsking.
-        </p>
-        </div>
-      </section>
-      <section></section>
+      <FirstItem />
+      <SecondItem/>
     </main>
   );
 };
