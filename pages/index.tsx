bc706595ecfead7f32ce7de972bb28790a70e106
Siham <siham.hadi@live.se>
2021-03-31 12:51:08 +0200

    Clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,35 @@
 import React, {useState, useRef, useEffect} from 'react';
-import FirstItem from '@components/First';
-import SecondItem from '@components/Second/index';
+import {GetServerSideProps} from 'next';
 
 import {Client} from '../prismic-configuration';
-import {GetServerSideProps} from 'next';
-import ThridView from '@components/Third';
-import Highlight from '@components/Highlight';
+import ClickOutsideRef from '../hooks/ClickedOutside';
+
+import FirstItem from '@components/First';
+import SecondItem from '@components/Second/index';
+import ThirdView from '@components/Third';
 import FourthView from '@components/Fourth/index';
+import Highlight from '@components/Highlight';
 import Footer from '@components/Footer';
 import SideMenu from '@components/SideMenu';
-import ClickOutsideRef from '../Hooks/ClickedOutside';
 import Spacer from '@components/Spacer';
 
 type Props = {
   content: any;
 };
+
 const Home = ({content}: Props) => {
-  const ref = useRef(null);
-  const [open, setOpen] = useState(false);
-  ClickOutsideRef(ref, () => setOpen(false));
   const header = 'Sömn';
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  ClickOutsideRef(ref, () => setOpen(false));
 
   useEffect(() => {
     open ? document.body.classList.add('overflow') : document.body.classList.remove('overflow');
-
     return () => {
       document.body.classList.remove('oveflow');
     };
   });
+
   return (
     <main className="main">
       <nav>
@@ -36,19 +38,15 @@ const Home = ({content}: Props) => {
 
         {open && <section ref={ref} className="overlay" />}
       </nav>
-
       <SideMenu open={open} closed={() => setOpen(!open)} />
       <Spacer size={1} />
       <FirstItem content={content} />
-      <Spacer size={10} />
-      <SecondItem content={content} />
-      <Spacer size={3} />
-      <ThridView content={content} />
-      <Spacer size={3} />
+      <Spacer size={1} />
+      <SecondItem content={content} /> <Spacer size={3} />
+      <ThirdView content={content} /> <Spacer size={3} />
       <FourthView content={content} />
       <Spacer size={5} />
-      <Highlight header={header} content={content} />
-      <Spacer size={10} />
+      <Highlight header={header} content={content} /> <Spacer size={10} />
       <Footer />
     </main>
   );
