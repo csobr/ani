a57724644fd98819d80a41a339179a0e691a99e0
Siham <siham.hadi@live.se>
2021-04-09 16:09:21 +0200

    Fix env

--- database/Firebase.ts
+++ database/Firebase.ts
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 let config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MSG_SENDER_ID,
