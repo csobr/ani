2562bee49136054be5e1413705342fe356e9f5c5
Siham Hadi <siham.hadi@live.se>
2021-05-11 14:35:37 +0200

    Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,8 @@ class MyDocument extends Document {
         <Head />
         <Head>
           <html lang="sv" />
-          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon.ico" />
+          <link rel="icon" href="icons/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon" />
           <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96" />
