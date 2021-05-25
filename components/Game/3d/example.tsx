256ca0de5e6aa4823e93e12e83acd7e09364b904
Siham Hadi <siham.hadi@live.se>
2021-05-25 21:33:31 +0200

    Add sky

--- components/Game/3d/example.tsx
+++ components/Game/3d/example.tsx
@@ -1,7 +1,24 @@
-import React, {Suspense, useRef} from 'react';
+import React, {Suspense, useRef, useEffect} from 'react';
 import * as THREE from 'three';
+import {Canvas, useFrame, useThree} from '@react-three/fiber';
+import {OrbitControls} from 'three-stdlib';
+import {Sky} from '@react-three/drei';
 
-import {Canvas, useFrame} from '@react-three/fiber';
+const CameraController = () => {
+  const {camera, gl} = useThree();
+
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+
+    controls.minDistance = 1;
+    controls.maxDistance = 20;
+
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
+};
 
 function Floor({args}) {
   const floor = useRef();
@@ -10,7 +27,7 @@ function Floor({args}) {
     floor.current.position.z += 0;
   });
   return (
-    <mesh visible position={[0, -100, 0]} rotation={[-Math.PI / 2, 0, 0]} ref={floor}>
+    <mesh visible position={[0, -200, 0]} rotation={[-Math.PI / 2, 0, 0]} ref={floor}>
       return (
       <planeBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial material="material" color="hotpink" />
@@ -45,24 +62,26 @@ function Square() {
 
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
   return (
-    <group position={[-5, -5, 10]}>
+    <group position={[-5, -5, 1]}>
       <line geometry={lineGeometry}>
-        <lineBasicMaterial attach="material" color="red" linewidth={1} linecap={'round'} linejoin={'round'} />
+        <lineBasicMaterial attach="material" color="black" linewidth={1} linecap={'round'} linejoin={'round'} />
       </line>
     </group>
   );
 }
 
+function SkyBox() {}
+
 const Scene = () => {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
-      <Canvas camera={{position: [0, 0, 20]}} style={{backgroundColor: 'black'}}>
-        <pointLight position={[10, 10, 10]} />
-        <ambientLight color="red" intensity={0.4} />
+      <Canvas camera={{position: [0, 0, 10]}} style={{backgroundColor: '#cce0ff'}}>
+        <CameraController />
+        <Sky sunPosition={[100, 10, 10, 100]} />
         <Suspense fallback={null}></Suspense>
 
         <Square />
-        <Triangel />
+
         {/* <Floor args={[5000, 3000, 10, 100]} /> */}
       </Canvas>
     </div>
